9794bac86b9749e6307a3db6e548f8caba2fe9ff
dwzm0 <dwzm00gmail.com>
2023-06-04 16:15:18 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import GlobalStyle from './globalStyle'
+
+const App = (): JSX.Element => {
+  return (
+    <>
+    <GlobalStyle />
+    <h1>HI THERE</h1>
+    </>
+  )
+}
+
+export default App
