ead9b66abe9ac0fe1d004d979d5fc89a56cdd516
dwzm0 <dwzm00gmail.com>
2023-06-27 20:28:08 +0300

    new router

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom'
 import './index.css'
+
+const router = createHashRouter([
+  {
+    path: '/*',
+    element: <App />
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 root.render(
     <React.StrictMode>
-        <App />
+         <RouterProvider router={router} />
     </React.StrictMode>
 )
