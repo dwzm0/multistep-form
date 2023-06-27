844e7ea45bb33896105943f554b266380bac104c
dwzm0 <dwzm00gmail.com>
2023-06-27 16:00:06 +0300

    hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 root.render(
-  <BrowserRouter basename="https://dwzm0.github.io/multistep-form">
     <React.StrictMode>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </React.StrictMode>
-  </BrowserRouter>
 )
